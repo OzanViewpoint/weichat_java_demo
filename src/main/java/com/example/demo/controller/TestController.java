50961134bf391784aa885a8d19e790296ead1442
Ozan <2678833496@qq.com>
2018-05-22 17:00:45 +0800

    添加注释，并更改存活标记

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -5,17 +5,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @Author: Ozan
- * @Description:
+ * @Description: 测试本项目是否存活
  * @Date: Created in 10:30 2018/5/3.
  * @Modified By:
  */
-@RestController
 
+@RestController
 public class TestController {
     @RequestMapping("/test")
     public String test() {
-        return "test";
+        return "active";
     }
-
-
 }
